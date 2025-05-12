bdb36e18df9a3dc5f62a12b77a5e364af6ebb2ee
Ruan Felssner <ruan_felssner@hotmail.com>
2025-05-11 23:22:11 -0700

    First commit

---
+++ - Deletar Logs -.bat
@@ -0,0 +1,22 @@
+@echo off
+
+del CashShopServer\LOG\* /q
+del ChatServer\Log\* /q
+del ConnectServer\Logs\* /q
+del DataServer\Item_Serial_Log\* /q
+del DataServer\Log\* /q
+del EventServer\LOG_ERROR\* /q
+del ExDB\LOG\* /q
+del ExDB\UNION_SYSTEM_LOG\* /q
+del GameServer\KUNDUN_EVENT_GM_LOG\* /q
+del GameServer\KUNDUN_EVENT_LOG\* /q
+del GameServer\LOG\* /q
+del GameServer\SQL_LOG\* /q
+del GameServerCS\CS_PCROOM_USER_LOG\* /q
+del GameServerCS\KUNDUN_EVENT_GM_LOG\* /q
+del GameServerCS\KUNDUN_EVENT_LOG\* /q
+del GameServerCS\LOG\* /q
+del GameServerCS\SQL_LOG\* /q
+del JoinServer\LOG\* /q
+del RankingServer\LOG_ERROR\* /q
+del RankingServer\LOG_POINT\* /q
